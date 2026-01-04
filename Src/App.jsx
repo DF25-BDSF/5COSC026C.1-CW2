7b24f9db5ab07e4c23879abe1442bc541f2c3c0c
DF25-BDSF <dulsha.20242188@iit.ac.lk>
2026-01-04 10:06:30 +0000

    fixed errors

--- Src/App.jsx
+++ Src/App.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SearchPage from './SearchPage';
 import PropertyPage from './PropertyPage';
-import '../App.css';
+import './App.css';
 
 export default function App() {
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/5COSC026C.1-CW2/' : '/'}>
       <div className="app">
         <header className="header">
           <h1>Estate Agent Property Search</h1>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/5COSC026C.1-CW2/' : '/',
   plugins: [react()],
   json: {
     stringify: false,
